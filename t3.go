7b280c71026f678d2dfcc0d2c16ee3e6b1fd2387
wuxin <opdss@qq.com>
2022-09-13 11:03:32 +0800

    tt

--- t3.go
+++ t3.go
@@ -2,26 +2,26 @@ package t3
 
 import "fmt"
 
-var T3 int
+var T31 int
 
 func init() {
 	fmt.Println("T3 init()")
-	T3 = 10
+	T31 = 10
 }
 
 func SetT3(val int) int {
 	fmt.Println("T3 SetT3(): t3 => %d", val)
-	T3 = val
-	return T3
+	T31 = val
+	return T31
 }
 
 func SetT3P100() int {
 	fmt.Println("T3 SetT3(): t3 => %d")
-	T3 += 100
-	return T3
+	T31 += 100
+	return T31
 }
 
 func GetT3() int {
-	fmt.Println("T3 GetT3(): t3 => %d", T3)
-	return T3
+	fmt.Println("T3 GetT3(): t3 => %d", T31)
+	return T31
 }
